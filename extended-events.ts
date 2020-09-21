e3167680f2b8a54a9a1c11683bdf98e1f9354dda
Nikhilesh Yadav <nikhileshofficial@gmail.com>
2020-09-21 21:08:54 +0530

    now compatible with new object-pattern-match module

--- extended-events.ts
+++ extended-events.ts
@@ -1,30 +1,31 @@
 import { EventEmitter } from "events";
-import match from "object-pattern-match"
+import  {match,pattern,target} from "object-pattern-match"
 /**
  * This class extends built-in EventEmitter Class to implement
  * - Object pattern based Event Identifiers instead of traditional strings
  * @requires object-pattern-match
  */
+export {pattern,target}
 export class EventEmitterExtended extends EventEmitter {
     /**
      * 
      * @param opts Options for EventEmitter super constructor
      */
-    #patterns: Map<Object, { (...args: any[]): void }>
-    #oneTime: Set<Object>
+    #patterns: Map<pattern, { (...args: any[]): void }>
+    #oneTime: Set<pattern>
     constructor(opts?: Object) {
         super(opts)
         this.#patterns = new Map
         this.#oneTime = new Set
         this.on = this.addListener
     }
-    emit(...arg: [Object | string | symbol, ...Array<any>]) {
+    emit(...arg: [Object | string | symbol| target, ...Array<any>]) {
         if (!(arg[0] instanceof Object)) return super.emit(...arg as [string |
             symbol, ...any[]])
         else {
             let hasemitted = false
             for (let [pattern, callback] of this.#patterns) {
-                if (match(pattern, arg[0])) {
+                if (match(pattern, arg[0] as target)) {
                     callback(arg[0]); hasemitted = true
                     if (this.#oneTime.has(pattern)) {
                         this.#oneTime.delete(pattern)
@@ -35,14 +36,14 @@ export class EventEmitterExtended extends EventEmitter {
             return hasemitted
         }
     }
-    addListener(type: Object | string | symbol, callback: { (...args: any[]): void }) {
+    addListener(type: pattern | string | symbol, callback: { (...args: any[]): void }) {
         if (!(type instanceof Object)) return super.addListener(type as string | symbol, callback)
         else {
             this.#patterns.set(type, callback)
             return this
         }
     }
-    removeListener(type: Object | string | symbol, callback: { (...args: any[]): void }) {
+    removeListener(type: pattern | string | symbol, callback: { (...args: any[]): void }) {
         if (!(type instanceof Object)) return super.removeListener(type as string | symbol, callback)
         else {
             if (this.#patterns.get(type) == callback)
@@ -50,7 +51,7 @@ export class EventEmitterExtended extends EventEmitter {
             return this
         }
     }
-    once(type: Object | string | symbol, callback: { (...args: any[]): void }) {
+    once(type: pattern | string | symbol, callback: { (...args: any[]): void }) {
         if (!(type instanceof Object)) return super.addListener(type as string | symbol, callback)
         else {
             this.#oneTime.add(type)
